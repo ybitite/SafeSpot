8b3434afe795380b0e016162f37a80730155b3cb
ybitite <younes.bitite@gmail.com>
2025-02-09 03:13:46 +0100

    fix: Correct Report data class to match API's JSON format
    This commit updates the `Report` data class to align with the
    API's expected JSON format. Key changes include:
    - Using `@SerializedName` annotations to map field names to
      `"Longitude"`, `"Latitude"`, `"Date_Time"`, `"Description"`,
      `"Image"`, and `"Video"`.
    - Changing the `date_time` field to a `String`.
    - Modifying the `setDate_time` method to format the date as
      `"yyyy-MM-dd'T'HH:mm:ss'Z'"` (with 'Z' for UTC).
    - Add the `toString()` method.
    
    These changes ensure that the data sent to the API is in the
    correct format, resolving the 400 Bad Request error.

---
+++ app/src/main/java/ch/y/bitite/safespot/repository/Report.java
@@ -0,0 +1,52 @@
+package ch.y.bitite.safespot.repository;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+public class Report {
+
+    @SerializedName("Longitude")
+    public double longitude;
+
+    @SerializedName("Latitude")
+    public double latitude;
+
+    @SerializedName("Date_Time")
+    public String date_time;
+
+    @SerializedName("Description")
+    public String description;
+
+    @SerializedName("Image")
+    public String image;
+
+    @SerializedName("Video")
+    public String video;
+
+    public Report() {
+    }
+
+    @Override
+    public String toString() {
+        return "Report{" +
+                "longitude=" + longitude +
+                ", latitude=" + latitude +
+                ", date_time='" + date_time + '\'' +
+                ", description='" + description + '\'' +
+                ", image='" + image + '\'' +
+                ", video='" + video + '\'' +
+                '}';
+    }
+
+    public void setDate_time(Date date_time) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault());
+        this.date_time = dateFormat.format(date_time);
+    }
+}
