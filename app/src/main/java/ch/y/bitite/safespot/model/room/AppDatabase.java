c6366a8fe1beb3afc4a59a8c72193096f5438a1a
ybitite <younes.bitite@gmail.com>
2025-02-09 03:20:33 +0100

    feat: Add ReportValidated entity to AppDatabase
    This commit adds the `ReportValidated` entity to the `AppDatabase`.
    This allows the Room database to store and manage validated reports
    fetched from the API.
    - Add the `ReportValidated` entity to the database.
    - Add the `ReportDao` to the database.

--- app/src/main/java/ch/y/bitite/safespot/model/room/AppDatabase.java
+++ app/src/main/java/ch/y/bitite/safespot/model/room/AppDatabase.java
@@ -9,8 +9,9 @@ import androidx.room.TypeConverters;
 import ch.y.bitite.safespot.model.ReportValidated;
 
 @Database(entities = {ReportValidated.class}, version = 1, exportSchema = false)
-@TypeConverters(Converters.class)
+@TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase {
+
     public abstract ReportDao reportDao();
 
     private static volatile AppDatabase INSTANCE;
@@ -19,8 +20,8 @@ public abstract class AppDatabase extends RoomDatabase {
         if (INSTANCE == null) {
             synchronized (AppDatabase.class) {
                 if (INSTANCE == null) {
-                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),AppDatabase.class, "report_database")
-                            .fallbackToDestructiveMigration()
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    AppDatabase.class, "app_database")
                             .build();
                 }
             }
