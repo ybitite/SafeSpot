82ea24610fa0224bf4c51c1c647ffedafbe4ba63
ybitite <younes.bitite@gmail.com>
2025-02-11 01:59:18 +0100

    fix: Add verification if the list is null in MapHelper Explanation: This commit message indicates that we've add a verification if the list is null in the MapHelper.

---
+++ app/src/main/java/ch/y/bitite/safespot/utils/MapHelper.java
@@ -0,0 +1,48 @@
+package ch.y.bitite.safespot.utils;
+
+import android.content.Context;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterManager;
+
+import java.util.List;
+
+import ch.y.bitite.safespot.model.ReportClusterItem;
+import ch.y.bitite.safespot.model.ReportValidated;
+import ch.y.bitite.safespot.ui.home.ReportRenderer;
+
+public class MapHelper {
+
+    private GoogleMap googleMap;
+    private ClusterManager<ReportClusterItem> clusterManager;
+    private Context context;
+
+    public MapHelper(Context context, GoogleMap map) {
+        this.context = context;
+        this.googleMap = map;
+        clusterManager = new ClusterManager<>(context, googleMap);
+        clusterManager.setRenderer(new ReportRenderer(context, googleMap, clusterManager));
+        googleMap.setOnCameraIdleListener(clusterManager);
+        googleMap.setOnMarkerClickListener(clusterManager);}
+
+    public void initializeMap(LatLng initialLocation) {
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(initialLocation, 13));
+    }
+
+    public void updateMapMarkers(List<ReportValidated> reports) {
+        if (reports != null) {
+            clusterManager.clearItems();
+            for (ReportValidated report : reports) {
+                ReportClusterItem item = new ReportClusterItem(report);
+                clusterManager.addItem(item);
+            }
+            clusterManager.cluster();
+        }
+    }
+
+    public void centerMap(LatLng location) {
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
+    }
+}
