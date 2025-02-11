0d28680597d2c02c12191fc46a7d52c956b0d640
ybitite <younes.bitite@gmail.com>
2025-02-11 02:35:01 +0100

    Refactor(DashboardViewModel): Remove report creation logic
    This commit removes report creation logic from DashboardViewModel, as this
    logic now resides in AddReportViewModel.
    
    - Removed report-related fields and methods.

--- app/src/main/java/ch/y/bitite/safespot/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/ch/y/bitite/safespot/ui/dashboard/DashboardFragment.java
@@ -5,9 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -15,13 +17,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import ch.y.bitite.safespot.R;
 import ch.y.bitite.safespot.databinding.FragmentDashboardBinding;
 
+
 public class DashboardFragment extends Fragment {
     private DashboardViewModel dashboardViewModel;
     private ReportAdapter adapter;
     private Button buttonAddReport;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState){
+                             ViewGroup container, Bundle savedInstanceState) {
         FragmentDashboardBinding binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
