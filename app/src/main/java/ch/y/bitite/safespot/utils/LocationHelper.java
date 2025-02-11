04eb62d9f8f39988c0ce2ba3d18dee88659c1bba
ybitite <younes.bitite@gmail.com>
2025-02-11 02:25:10 +0100

    Feat(LocationHelper): Add stopLocationUpdates() method
    This commit adds the stopLocationUpdates() method to LocationHelper,
    allowinglocation updates to be stopped when no longer needed.
    
    - Implemented stopLocationUpdates() in LocationHelper.

--- app/src/main/java/ch/y/bitite/safespot/utils/LocationHelper.java
+++ app/src/main/java/ch/y/bitite/safespot/utils/LocationHelper.java
@@ -1,9 +1,10 @@
 package ch.y.bitite.safespot.utils;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Looper;
+import android.util.Log;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -11,64 +12,98 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
 import com.google.android.gms.maps.model.LatLng;
 
 public class LocationHelper {
 
-    private final FusedLocationProviderClient fusedLocationClient;
-    private final ActivityResultLauncher<String[]> locationPermissionRequest;
-    private LocationCallback locationCallback;
-    private Context context;
+    private static final String TAG = "LocationHelper";
+    private final Fragment fragment;
+    private final LocationCallback externalLocationCallback;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationRequest locationRequest;
+    private com.google.android.gms.location.LocationCallback internalLocationCallback;
 
     public interface LocationCallback {
         void onLocationResult(LatLng location);
     }
 
     public LocationHelper(Fragment fragment, LocationCallback callback) {
-        this.context = fragment.requireContext();
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
-        locationCallback = callback;
-        locationPermissionRequest = fragment.registerForActivityResult(
-                new ActivityResultContracts.RequestMultiplePermissions(),
-                permissions -> {
-                    if (Boolean.TRUE.equals(permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false)) ||
-                            Boolean.TRUE.equals(permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false))) {
-                        // Permission is granted. Continue the action or workflow in your app.
-                        getCurrentLocation();
-                    } else {
-                        // Explain to the user that the feature is unavailable because the
-                        // features requires a permission that the user has denied.
-                    }
+        this.fragment = fragment;
+        this.externalLocationCallback = callback;
+        this.fusedLocationClient = LocationServices.getFusedLocationProviderClient(fragment.requireContext());
+        this.locationRequest = createLocationRequest();
+        this.internalLocationCallback = createInternalLocationCallback();
+    }
+
+    private LocationRequest createLocationRequest() {
+        return new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 10000)
+                .setWaitForAccurateLocation(false)
+                .setMinUpdateIntervalMillis(5000)
+                .setMinUpdateDistanceMeters(10)
+                .build();
+    }
+
+    private com.google.android.gms.location.LocationCallback createInternalLocationCallback() {
+        return new com.google.android.gms.location.LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
                 }
-        );
+                for (Location location : locationResult.getLocations()) {
+                    Log.d(TAG, "Location: " + location.getLatitude() + ", " + location.getLongitude());
+                    externalLocationCallback.onLocationResult(new LatLng(location.getLatitude(), location.getLongitude()));
+                }
+            }
+        };
     }
 
     public void checkLocationPermissions() {
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // Demander les permissions
-            locationPermissionRequest.launch(new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-            });
+        if (ActivityCompat.checkSelfPermission(fragment.requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(fragment.requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestLocationPermission();
         } else {
-            // Les permissions sont déjà accordées
-            getCurrentLocation();
+            requestLocationUpdates();
         }
     }
 
-    private void getCurrentLocation() {
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+    private void requestLocationPermission() {
+        ActivityResultLauncher<String[]> locationPermissionRequest =
+                fragment.registerForActivityResult(new ActivityResultContracts
+                                .RequestMultiplePermissions(), result -> {
+                            Boolean fineLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+                            Boolean coarseLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_COARSE_LOCATION, false);
+                            if (fineLocationGranted != null && fineLocationGranted) {
+                                // Preciselocation access granted.
+                                requestLocationUpdates();
+                            } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                                // Only approximate location access granted.
+                                requestLocationUpdates();
+                            } else {
+                                // No location access granted.
+                                externalLocationCallback.onLocationResult(null);
+                            }
+                        }
+                );
+        locationPermissionRequest.launch(new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        });
+    }
+
+    private void requestLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(fragment.requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(fragment.requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        fusedLocationClient.getLastLocation()
-                .addOnSuccessListener(location -> {
-                    if (location != null) {
-                        LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                        locationCallback.onLocationResult(currentLocation);
-                    }
-                });
+        fusedLocationClient.requestLocationUpdates(locationRequest, internalLocationCallback, Looper.getMainLooper());
+    }
+
+    public void stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(internalLocationCallback);
     }
 }
