1e8bc9036719378755953bac402983c4dc9bb1d6
ybitite <younes.bitite@gmail.com>
2025-02-13 04:32:30 +0100

    Feat: Implement image upload for reports
    - Added the ability to upload images when creating reports.
    - Updated the API service to handle multipart requests for report creation, including image, description, longitude, latitude, and date-time.

--- app/src/main/java/ch/y/bitite/safespot/repository/ApiService.java
+++ app/src/main/java/ch/y/bitite/safespot/repository/ApiService.java
@@ -2,17 +2,26 @@ package ch.y.bitite.safespot.repository;
 
 import java.util.List;
 
-import ch.y.bitite.safespot.model.Report;
 import ch.y.bitite.safespot.model.ReportValidated;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface ApiService {
     @GET("api/reports")
     Call<List<ReportValidated>> getValidatedReports();
 
+    @Multipart
     @POST("api/reports")
-    Call<Void> addReport(@Body Report newReport);
+    Call<Void> addReport(
+            @Part MultipartBody.Part image,
+            @Part("Description") RequestBody description,
+            @Part("Longitude") RequestBody longitude,
+            @Part("Latitude") RequestBody latitude,
+            @Part("Date_Time") RequestBody dateTime
+    );
 }
