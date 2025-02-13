41feb7503611f4e436a254d0ed8408fcef11e67c
ybitite <younes.bitite@gmail.com>
2025-02-13 17:02:41 +0100

    Refactor: Update report validated date fields
    - Changed `date_time` field type from String to Date.
    - Changed `date_time_validation` field type from String to Date.

--- app/src/main/java/ch/y/bitite/safespot/model/ReportValidated.java
+++ app/src/main/java/ch/y/bitite/safespot/model/ReportValidated.java
@@ -3,23 +3,21 @@ package ch.y.bitite.safespot.model;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity(tableName = "reports_validated")
 public class ReportValidated {
-
     @PrimaryKey(autoGenerate = true)
     public int id;
 
     public double longitude;
     public double latitude;
-    public String date_time;
+    public Date date_Time; // Changed to date_Time
     public String description;
     public String image;
     public String video;
     public String comment;
-    public String date_time_validation;
+    public Date date_Time_Validation; // Changed to date_Time_Validation
 
     public ReportValidated() {
     }
