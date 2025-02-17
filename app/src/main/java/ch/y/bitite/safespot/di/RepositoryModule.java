ffcd5791277a2d91db9ad306124f4214c911dcc5
ybitite <younes.bitite@gmail.com>
2025-02-18 00:36:13 +0100

    Feat: Implement RepositoryModule for data sources
    - Added RepositoryModule to provide dependencies for data sources.
    - Implemented providers for ApiService, ExecutorService, ReportLocalDataSource, ReportRemoteDataSource, and ReportFileDataSource.
    - Implemented ReportRepository provider.

---
+++ app/src/main/java/ch/y/bitite/safespot/di/RepositoryModule.java
@@ -0,0 +1,62 @@
+package ch.y.bitite.safespot.di;
+
+import android.content.Context;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.inject.Singleton;
+
+import ch.y.bitite.safespot.model.room.ReportDao;
+import ch.y.bitite.safespot.network.ApiService;
+import ch.y.bitite.safespot.repository.ReportFileDataSource;
+import ch.y.bitite.safespot.repository.ReportLocalDataSource;
+import ch.y.bitite.safespot.repository.ReportRemoteDataSource;
+import ch.y.bitite.safespot.repository.ReportRepository;
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.qualifiers.ApplicationContext;
+import dagger.hilt.components.SingletonComponent;
+import retrofit2.Retrofit;
+
+@Module
+@InstallIn(SingletonComponent.class)
+public class RepositoryModule {
+
+    @Provides
+    @Singleton
+    public ApiService provideApiService(Retrofit retrofit) {
+        return retrofit.create(ApiService.class);
+    }
+
+    @Provides
+    @Singleton
+    public ExecutorService provideExecutorService() {
+        return Executors.newSingleThreadExecutor();
+    }
+
+    @Provides
+    @Singleton
+    public ReportLocalDataSource provideReportLocalDataSource(ReportDao reportDao, ExecutorService executorService) {
+        return new ReportLocalDataSource(reportDao, executorService);
+    }
+
+    @Provides
+    @Singleton
+    public ReportRemoteDataSource provideReportRemoteDataSource(ApiService apiService) {
+        return new ReportRemoteDataSource(apiService);
+    }
+
+    @Provides
+    @Singleton
+    public ReportFileDataSource provideReportFileDataSource(@ApplicationContext Context context) {
+        return new ReportFileDataSource(context);
+    }
+
+    @Provides
+    @Singleton
+    public ReportRepository provideReportRepository(ReportLocalDataSource localDataSource, ReportRemoteDataSource remoteDataSource, ReportFileDataSource fileDataSource) {
+        return new ReportRepository(localDataSource, remoteDataSource, fileDataSource);
+    }
+}
