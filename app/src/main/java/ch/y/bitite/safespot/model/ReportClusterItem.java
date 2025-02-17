54c559c29f1ea30be4abee445bfa0b87600ee741
ybitite <younes.bitite@gmail.com>
2025-02-18 00:35:38 +0100

    Feat: Add snippet and zIndex to ReportClusterItem
    - Added a snippet to the `ReportClusterItem` to display the report's date and time.
    - Added a `zIndex` to the `ReportClusterItem`, set to 0.0f.
    - Used the Report fields `latitude`, `longitude`, and `description` to set the cluster item.
    - Added the method `getDateTimeString` to convert the report timestamp to a `String` to be displayed.

--- app/src/main/java/ch/y/bitite/safespot/model/ReportClusterItem.java
+++ app/src/main/java/ch/y/bitite/safespot/model/ReportClusterItem.java
@@ -1,18 +1,31 @@
 package ch.y.bitite.safespot.model;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.clustering.ClusterItem;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class ReportClusterItem implements ClusterItem {
-    private final ReportValidated report;
     private final LatLng position;
     private final String title;
+    private final String snippet;
+    private final float zIndex;
 
     public ReportClusterItem(ReportValidated report) {
-        this.report = report;
-        this.position = new LatLng(report.latitude, report.longitude);
-        this.title = report.description;
+        this.position = new LatLng(report.getLatitude(), report.getLongitude());
+        this.title = report.getDescription();
+        this.snippet = report.getDateTimeString();
+        this.zIndex = 0.0f;
     }
+
+
 
     @Override
     public LatLng getPosition() {
@@ -26,11 +39,11 @@ public class ReportClusterItem implements ClusterItem {
 
     @Override
     public String getSnippet() {
-        return null;
+        return snippet;
     }
 
     @Override
     public Float getZIndex() {
-        return 0.0f;
+        return zIndex;
     }
 }
