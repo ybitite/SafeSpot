c830120a61ab8d65e81aad74ed5e6c62ee9fd948
ybitite <younes.bitite@gmail.com>
2025-02-18 00:26:54 +0100

    Feat: Add Hilt plugin
    - Added the Hilt plugin to the top-level build file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    id("com.google.dagger.hilt.android") version "2.55" apply false
+
 }
