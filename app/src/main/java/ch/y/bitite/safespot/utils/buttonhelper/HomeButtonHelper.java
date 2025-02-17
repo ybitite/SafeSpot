ba2eff31e85bf85a4bab43f387e5fc4b7fd32b51
ybitite <younes.bitite@gmail.com>
2025-02-18 00:34:29 +0100

    Feat: Introduce HomeButtonHelper for button management
    - Created HomeButtonHelper to manage button interactions in HomeFragment.
    - Implemented callbacks for "Add Report" and "Center Map" button clicks.
    - Set up button listeners in HomeFragment using the new helper class.

---
+++ app/src/main/java/ch/y/bitite/safespot/utils/buttonhelper/HomeButtonHelper.java
@@ -0,0 +1,35 @@
+package ch.y.bitite.safespot.utils.buttonhelper;
+
+import android.view.View;
+import android.widget.Button;
+
+import ch.y.bitite.safespot.R;
+import ch.y.bitite.safespot.ui.home.HomeFragment;
+
+public class HomeButtonHelper {
+
+    private Button buttonAddReportHome;
+    private Button buttonCenterMap;
+    private HomeButtonCallback homeButtonCallback;
+
+    public interface HomeButtonCallback {
+        void onCenterMapClicked();
+        void onAddReportClicked();
+    }
+
+    public HomeButtonHelper(View root, HomeButtonCallback callback) {
+        this.homeButtonCallback = callback;
+        buttonAddReportHome = root.findViewById(R.id.buttonAddReportHome);
+        buttonCenterMap = root.findViewById(R.id.buttonCenterMap);
+    }
+
+    public void setupHomeButtonListeners(HomeFragment homeFragment) {
+        buttonAddReportHome.setOnClickListener(v -> {
+            homeButtonCallback.onAddReportClicked();
+        });
+
+        buttonCenterMap.setOnClickListener(v -> {
+            homeButtonCallback.onCenterMapClicked();
+        });
+    }
+}
