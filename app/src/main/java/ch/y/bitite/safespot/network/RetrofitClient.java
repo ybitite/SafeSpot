b1326f22f530300c6e5a56b8979368d57dce41bb
ybitite <younes.bitite@gmail.com>
2025-02-18 00:36:19 +0100

    Refactor: Improve Retrofit client setup
    - Moved Retrofit, OkHttpClient, and HttpLoggingInterceptor creation into dedicated methods.
    - Removed the static retrofit instance and the related logic.
    - Removed DateTypeAdapter as no longer required.
    - Added a method to create Gson instance.
    - Used BuildConfig.BASE_URL for better management.

--- app/src/main/java/ch/y/bitite/safespot/network/RetrofitClient.java
+++ app/src/main/java/ch/y/bitite/safespot/network/RetrofitClient.java
@@ -5,43 +5,37 @@ import android.util.Log;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import ch.y.bitite.safespot.utils.DateTypeAdapter;
+import ch.y.bitite.safespot.BuildConfig;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import java.util.Date;
-
 public class RetrofitClient {
-    private static final String BASE_URL = "https://safespotapi20250207214631.azurewebsites.net/";
-    private static Retrofit retrofit;
     private static final String TAG = "RetrofitClient";
 
-    public static Retrofit getInstance() {
-        if (retrofit == null) {
-            HttpLoggingInterceptor logging = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
-                @Override
-                public void log(String message) {
-                    Log.d(TAG, "log: " + message);
-                }
-            });
-            logging.setLevel(HttpLoggingInterceptor.Level.BODY); // Log request and response bodies
+    public static Retrofit createRetrofit(OkHttpClient client, Gson gson) {
+        return new Retrofit.Builder()
+                .baseUrl(BuildConfig.BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+    }
 
-            OkHttpClient client = new OkHttpClient.Builder()
-                    .addInterceptor(logging)
-                    .build();
+    public static OkHttpClient createOkHttpClient(HttpLoggingInterceptor logging) {
+        return new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+    }
 
-            Gson gson = new GsonBuilder()
-                    .registerTypeAdapter(Date.class, new DateTypeAdapter())
-                    .create();
+    public static HttpLoggingInterceptor createLoggingInterceptor() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor(message -> Log.d(TAG, "log: " + message));
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        return logging;
+    }
 
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .client(client) // Add the OkHttpClient with the interceptor
-                    .addConverterFactory(GsonConverterFactory.create(gson))
-                    .build();
-        }
-        return retrofit;
+    public static Gson createGson() {
+        return new GsonBuilder()
+                .create();
     }
 }
