5ba925c15a0abba2bae0fa24f7305b9236d621a2
ybitite <younes.bitite@gmail.com>
2025-02-18 00:35:43 +0100

    Refactor(ReportDao): Enhance database operations for validated reports
    - Added methods to retrieve, update, and delete lists of validated reports.
    - Added a method to retrieve all validated reports without using LiveData.
    - Improved Javadoc documentation for clarity and completeness.

--- app/src/main/java/ch/y/bitite/safespot/model/room/ReportDao.java
+++ app/src/main/java/ch/y/bitite/safespot/model/room/ReportDao.java
@@ -2,23 +2,67 @@ package ch.y.bitite.safespot.model.room;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
 import ch.y.bitite.safespot.model.ReportValidated;
 
+/**
+ * Data Access Object (DAO) for the ReportValidated entity.
+ * This interface provides methods to interact with the 'reports_validated' table in the database.
+ */
 @Dao
 public interface ReportDao {
 
+    /**
+     * Retrieves all validated reports from the database, ordered by ID in descending order.
+     *
+     * @return A LiveData list of ReportValidated objects.
+     */
     @Query("SELECT * FROM reports_validated ORDER BY id DESC")
     LiveData<List<ReportValidated>> getAllValidatedReports();
 
+    /**
+     * Retrieves all validated reports from the database.
+     *
+     * @return A list of ReportValidated objects.
+     */
+    @Query("SELECT * FROM reports_validated")
+    List<ReportValidated> getAllValidatedReportsNotLiveData();
+
+    /**
+     * Inserts a list of validated reports into the database.
+     * If a report with the same ID already exists, it will be ignored.
+     *
+     * @param reports The list of ReportValidated objects to insert.
+     */
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insertValidatedReports(List<ReportValidated> reports);
 
+    /**
+     * Update a list of validated reports in the database.
+     *
+     * @param reports The list of ReportValidated objects to update.
+     */
+    @Update
+    void updateValidatedReports(List<ReportValidated> reports);
+
+    /**
+     * Delete a list of validated reports in the database.
+     *
+     * @param reports The list of ReportValidated objects to delete.
+     */
+    @Delete
+    void deleteValidatedReports(List<ReportValidated> reports);
+
+    /**
+     * Deletes all validated reports from the database.
+     */
     @Query("DELETE FROM reports_validated")
     void deleteAllValidatedReports();
 }
