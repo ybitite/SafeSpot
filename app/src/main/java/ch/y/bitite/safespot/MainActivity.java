4824c5433295394c6f1c152ee999c180e27bf7a4
ybitite <younes.bitite@gmail.com>
2025-02-18 00:34:56 +0100

    Refactor: Migrate navigation graph and add Hilt entry point
    - Updated the navigation graph IDs to `homeFragment` and `dashboardFragment`.
    - Added `@AndroidEntryPoint` annotation to `MainActivity`.

--- app/src/main/java/ch/y/bitite/safespot/MainActivity.java
+++ app/src/main/java/ch/y/bitite/safespot/MainActivity.java
@@ -12,6 +12,9 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import ch.y.bitite.safespot.databinding.ActivityMainBinding;
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint // Ajouter l'annotation ici
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard)
+                R.id.homeFragment, R.id.dashboardFragment)
                 .build();
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.nav_host_fragment_activity_main);
