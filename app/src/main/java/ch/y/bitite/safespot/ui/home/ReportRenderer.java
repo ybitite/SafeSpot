82c383f11d2eb9d0c8b8b779f1bb2127e32f61b1
ybitite <younes.bitite@gmail.com>
2025-02-18 02:03:22 +0100

    Feat: Enhance cluster and marker rendering with custom icons and colors
    - Added comments for improved code understanding.

--- app/src/main/java/ch/y/bitite/safespot/ui/home/ReportRenderer.java
+++ app/src/main/java/ch/y/bitite/safespot/ui/home/ReportRenderer.java
@@ -2,11 +2,11 @@ package ch.y.bitite.safespot.ui.home;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
@@ -14,6 +14,7 @@ import androidx.core.content.ContextCompat;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.clustering.Cluster;
 import com.google.maps.android.clustering.ClusterManager;
@@ -22,81 +23,144 @@ import com.google.maps.android.clustering.view.DefaultClusterRenderer;
 import ch.y.bitite.safespot.R;
 import ch.y.bitite.safespot.model.ReportClusterItem;
 
+/**
+ * This class manages the custom display of clusters and markers on the map.
+ */
 public class ReportRenderer extends DefaultClusterRenderer<ReportClusterItem> {
-    private final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private BitmapDescriptor customMarkerIcon;
-    private Context context;
-    private final Paint haloPaint; // Paint for the halo
+    private static final String TAG = "ReportRenderer";
 
+    private final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private final BitmapDescriptor customMarkerIcon;
+    private final Paint haloPaint;
+    private final Context context;
+
+    /**
+     * Constructor for ReportRenderer.
+     *
+     * @param context        The application context.
+     * @param map            The GoogleMap object.
+     * @param clusterManager The associated ClusterManager.
+     */
     public ReportRenderer(Context context, GoogleMap map, ClusterManager<ReportClusterItem> clusterManager) {
         super(context, map, clusterManager);
         this.context = context;
+
+        // Initialize the paint for the text in the clusters.
         paint.setColor(Color.WHITE);
         paint.setTextAlign(Paint.Align.CENTER);
         paint.setTextSize(30f);
 
-        // Initialize the halo paint
+        // Initialize the paint for the halo around the clusters.
         haloPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        haloPaint.setStyle(Paint.Style.FILL); // Set the style tofill for a filled shape
+        haloPaint.setStyle(Paint.Style.FILL);
 
-        // Load the custom marker icon
+        // Load the custom icon for individual markers.
         Bitmap originalBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.emergency_icon);
-        int newWidth = 80;
-        int newHeight = 80;
-        Bitmap resizedBitmap = Bitmap.createScaledBitmap(originalBitmap, newWidth, newHeight, false);
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(originalBitmap, 80, 80, false);
         customMarkerIcon = BitmapDescriptorFactory.fromBitmap(resizedBitmap);
     }
 
+    /**
+     * Called before rendering an individual item (not clustered).
+     *
+     * @param item          The item to render.
+     * @param markerOptions The marker options.
+     */
     @Override
     protected void onBeforeClusterItemRendered(@NonNull ReportClusterItem item, @NonNull MarkerOptions markerOptions) {
+        Log.d(TAG, "onBeforeClusterItemRendered: Rendering a single item");
         markerOptions.title(item.getTitle());
-        markerOptions.icon(customMarkerIcon);
+        markerOptions.snippet(item.getSnippet());
+        // We no longer set the icon here because we do it in onClusterItemRendered
     }
 
+    /**
+     * Called after rendering an individual item (not clustered).
+     *
+     * @param clusterItem The rendered item.
+     * @param marker      The rendered marker.
+     */
+    @Override
+    protected void onClusterItemRendered(@NonNull ReportClusterItem clusterItem, @NonNull Marker marker) {
+        super.onClusterItemRendered(clusterItem, marker);
+        // We set the icon here to make sure it is properly set
+        marker.setIcon(customMarkerIcon);
+    }
+
+    /**
+     * Called before rendering a cluster.
+     *
+     * @param cluster       The cluster to render.
+     * @param markerOptions The marker options.
+     */
     @Override
     protected void onBeforeClusterRendered(@NonNull Cluster<ReportClusterItem> cluster, @NonNull MarkerOptions markerOptions) {
+        Log.d(TAG, "onBeforeClusterRendered: Rendering a cluster");
         int clusterSize = cluster.getSize();
-        int color;
-        if (clusterSize <= 5) {
-            color = ContextCompat.getColor(context, R.color.green);
-        } else if (clusterSize <= 10) {
-            color = ContextCompat.getColor(context, R.color.yellow);
-        } else {
-            color = ContextCompat.getColor(context, R.color.red);
-        }
-        BitmapDescriptor icon = createClusterIcon(clusterSize, color);
-        markerOptions.icon(icon);
+        int color = getClusterColor(clusterSize);
+        markerOptions.icon(createClusterIcon(clusterSize, color));
     }
 
+    /**
+     * Called to determine if a cluster should be rendered or not.
+     *
+     * @param cluster The cluster to check.
+     * @return True if the cluster should be rendered, false otherwise.
+     */
     @Override
     protected boolean shouldRenderAsCluster(@NonNull Cluster<ReportClusterItem> cluster) {
+        // We display a cluster if there is more than one element
         return cluster.getSize() > 1;
     }
 
+    /**
+     * Creates the custom icon for a cluster.
+     *
+     * @param clusterSize The size of the cluster.
+     * @param color       The color of the cluster.
+     * @return The custom icon (BitmapDescriptor).
+     */
     private BitmapDescriptor createClusterIcon(int clusterSize, int color) {
-        int circleRadius = 50;
-        int outlineRadius = circleRadius * 2; // Double the radius for the outline
-        int bitmapSize = outlineRadius * 2; // Size of the bitmap to accommodate the outline
+        Log.d(TAG, "createClusterIcon: Creating cluster icon for size: " + clusterSize);
+        int radius = 50;
+        int outlineRadius = radius * 2;
+        int size = outlineRadius * 2;
 
-        Bitmap bitmap = Bitmap.createBitmap(bitmapSize, bitmapSize, Bitmap.Config.ARGB_8888);
+        Bitmap bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
 
-        // Draw the transparent halo
+        // Draw the transparent halo.
         haloPaint.setColor(color);
-        haloPaint.setAlpha(80); // Set the transparency (0-255, 80 is about 30% transparent)
-        canvas.drawCircle(bitmapSize / 2,bitmapSize / 2, outlineRadius, haloPaint);
+        haloPaint.setAlpha(80);
+        canvas.drawCircle(size / 2, size / 2, outlineRadius, haloPaint);
 
-        // Draw the inner circle
+        // Draw the inner circle.
         Paint paintCircle = new Paint(Paint.ANTI_ALIAS_FLAG);
         paintCircle.setColor(color);
-        canvas.drawCircle(bitmapSize / 2, bitmapSize / 2, circleRadius, paintCircle);
+        canvas.drawCircle(size / 2, size / 2, radius, paintCircle);
 
-        // Draw the text
+        // Draw the text (cluster size).
         String text = String.valueOf(clusterSize);
-        float textX = bitmapSize / 2;
-        float textY = bitmapSize / 2 - (paint.descent() + paint.ascent()) / 2;
+        float textX = size / 2;
+        float textY = size / 2 - (paint.descent() + paint.ascent()) / 2;
         canvas.drawText(text, textX, textY, paint);
 
         return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
+    /**
+     * Determines the color of the cluster based on its size.
+     *
+     * @param clusterSize The size of the cluster.
+     * @return The color of the cluster.
+     */
+    private int getClusterColor(int clusterSize) {
+        if (clusterSize <= 5) {
+            return ContextCompat.getColor(context, R.color.green);
+        } else if (clusterSize <= 10) {
+            return ContextCompat.getColor(context, R.color.yellow);
+        } else {
+            return ContextCompat.getColor(context, R.color.red);
+        }
     }
 }
