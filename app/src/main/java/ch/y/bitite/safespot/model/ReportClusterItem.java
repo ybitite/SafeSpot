d8a03f1bf554c996f58d135bf1e09066764d2258
ybitite <younes.bitite@gmail.com>
2025-02-19 23:11:16 +0100

    Feat: Add validated report field to ReportClusterItem

--- app/src/main/java/ch/y/bitite/safespot/model/ReportClusterItem.java
+++ app/src/main/java/ch/y/bitite/safespot/model/ReportClusterItem.java
@@ -1,5 +1,7 @@
 package ch.y.bitite.safespot.model;
 
+import androidx.annotation.NonNull;
+
 import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.clustering.ClusterItem;
 
@@ -11,6 +13,12 @@ public class ReportClusterItem implements ClusterItem {
     private final String date;
     private final String imageFileName;
 
+    public ReportValidated getReportValidated() {
+        return reportValidated;
+    }
+
+    private final ReportValidated reportValidated;
+
 
     public ReportClusterItem(ReportValidated report) {
         this.position = new LatLng(report.getLatitude(), report.getLongitude());
@@ -19,6 +27,7 @@ public class ReportClusterItem implements ClusterItem {
         this.zIndex = 0.0f;
         this.date = report.getDateTimeString();
         this.imageFileName = report.getImage();
+        reportValidated = report;
     }
 
     @Override
@@ -45,9 +54,6 @@ public class ReportClusterItem implements ClusterItem {
         return date;
     }
 
-    public int getImage() {
-        return 0;
-    }
 
     public String getImageFileName() {
         return imageFileName;
