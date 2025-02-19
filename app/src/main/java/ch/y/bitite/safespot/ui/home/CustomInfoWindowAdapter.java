0444d7959bbc98534a568f3024d28fa062e8316d
ybitite <younes.bitite@gmail.com>
2025-02-19 23:10:54 +0100

    Refactor: Improve CustomInfoWindowAdapter for map markers

--- app/src/main/java/ch/y/bitite/safespot/ui/home/CustomInfoWindowAdapter.java
+++ app/src/main/java/ch/y/bitite/safespot/ui/home/CustomInfoWindowAdapter.java
@@ -1,9 +1,6 @@
 package ch.y.bitite.safespot.ui.home;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.text.SpannableString;
-import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,15 +9,15 @@ import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
+import com.google.maps.android.clustering.ClusterManager;
 
 import ch.y.bitite.safespot.R;
-import ch.y.bitite.safespot.model.ReportValidated;
+import ch.y.bitite.safespot.model.ReportClusterItem;
 import ch.y.bitite.safespot.utils.ImageLoader;
 
 public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
     private final View mWindow;
-
     private final Context context;
     private final ImageLoader imageLoader;
 
@@ -43,28 +40,27 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
     }
 
     private void render(Marker marker, View view) {
-        // Corrected line: Cast to ReportValidated instead of ReportClusterItem
-        ReportValidated reportValidated = (ReportValidated) marker.getTag();
+        // Get the ReportClusterItem from the Marker's tag
+        ReportClusterItem reportClusterItem = (ReportClusterItem) marker.getTag();
 
-        if (reportValidated != null) {
+        if (reportClusterItem != null) {
             TextView titleUi = view.findViewById(R.id.info_window_title);
             TextView descriptionUi = view.findViewById(R.id.info_window_date);
             ImageView imageUi = view.findViewById(R.id.info_window_image);
-
-            String title = reportValidated.getDescription();
+            String title = reportClusterItem.getTitle();
             if (title != null) {
                 titleUi.setText(title);
             } else {
                 titleUi.setText("");
             }
 
-            String description = reportValidated.getDateTimeString();
-            if (description != null) {
-                descriptionUi.setText(description);
+            String date = reportClusterItem.getDate();
+            if (date != null) {
+                descriptionUi.setText(date);
             } else {
                 descriptionUi.setText("");
             }
-            imageLoader.loadImage(reportValidated.getImage(), imageUi, new ImageLoader.ImageLoadCallback() {
+            imageLoader.loadImage(reportClusterItem.getImageFileName(), imageUi, new ImageLoader.ImageLoadCallback() {
                 @Override
                 public void onImageLoaded() {
                     if (marker.isInfoWindowShown()) {
