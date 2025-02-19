bb8d53ae8905016bd094b55108ac99228923628f
ybitite <younes.bitite@gmail.com>
2025-02-19 02:28:13 +0100

    Refactor: Update ReportClusterItem
    - Added `date` and `imageFileName` properties to `ReportClusterItem`.
    - Removed unused code from `ReportClusterItem`.
    - Added methods `getDate`, `getImage` and `getImageFileName` to `ReportClusterItem`.

--- app/src/main/java/ch/y/bitite/safespot/model/ReportClusterItem.java
+++ app/src/main/java/ch/y/bitite/safespot/model/ReportClusterItem.java
@@ -1,31 +1,25 @@
 package ch.y.bitite.safespot.model;
 
-import android.os.Build;
-
-import androidx.annotation.RequiresApi;
-
 import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.clustering.ClusterItem;
-
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
 public class ReportClusterItem implements ClusterItem {
     private final LatLng position;
     private final String title;
     private final String snippet;
     private final float zIndex;
+    private final String date;
+    private final String imageFileName;
+
 
     public ReportClusterItem(ReportValidated report) {
         this.position = new LatLng(report.getLatitude(), report.getLongitude());
         this.title = report.getDescription();
         this.snippet = report.getDateTimeString();
         this.zIndex = 0.0f;
+        this.date = report.getDateTimeString();
+        this.imageFileName = report.getImage();
     }
-
-
 
     @Override
     public LatLng getPosition() {
@@ -45,5 +39,17 @@ public class ReportClusterItem implements ClusterItem {
     @Override
     public Float getZIndex() {
         return zIndex;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int getImage() {
+        return 0;
+    }
+
+    public String getImageFileName() {
+        return imageFileName;
     }
 }
