65911c5a838e096ba3aecaa65df3b84be1ed038c
ybitite <younes.bitite@gmail.com>
2025-02-19 18:46:15 +0100

    Refactor: Improve Info Window Content Display
    - Changed the data model in the CustomInfoWindowAdapter from ReportClusterItem to ReportValidated.
    - Modified the rendering logic in CustomInfoWindowAdapter to display description and date from ReportValidated objects.
    - Added error handling for cases where the reportValidated object is null or image loading fails.
    - Updated the logic to refresh the info window after the image is loaded.

--- app/src/main/java/ch/y/bitite/safespot/ui/home/CustomInfoWindowAdapter.java
+++ app/src/main/java/ch/y/bitite/safespot/ui/home/CustomInfoWindowAdapter.java
@@ -1,57 +1,85 @@
 package ch.y.bitite.safespot.ui.home;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
 
 import ch.y.bitite.safespot.R;
-import ch.y.bitite.safespot.model.ReportClusterItem;
+import ch.y.bitite.safespot.model.ReportValidated;
 import ch.y.bitite.safespot.utils.ImageLoader;
 
 public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
-    private final ImageLoader imageLoader;
-    private View infoWindowView;
+    private final View mWindow;
+
     private final Context context;
+    private final ImageLoader imageLoader;
 
     public CustomInfoWindowAdapter(Context context) {
         this.context = context;
         this.imageLoader = new ImageLoader(context);
-        this.infoWindowView = LayoutInflater.from(context).inflate(R.layout.info_window_layout, null);
+        mWindow = LayoutInflater.from(context).inflate(R.layout.info_window_layout, null);
     }
 
     @Override
     public View getInfoWindow(Marker marker) {
-        // Get the ReportClusterItem associated with the marker
-        ReportClusterItem reportClusterItem = (ReportClusterItem) marker.getTag();
-
-        // Find the views in the layout
-        ImageView imageView = infoWindowView.findViewById(R.id.info_window_image);
-        TextView titleTextView = infoWindowView.findViewById(R.id.info_window_title);
-        TextView snippetTextView = infoWindowView.findViewById(R.id.info_window_snippet);
-        TextView dateTextView = infoWindowView.findViewById(R.id.info_window_date);
-
-        // Set the data to the views
-        if (reportClusterItem != null) {
-            titleTextView.setText(reportClusterItem.getTitle());
-            snippetTextView.setText(reportClusterItem.getSnippet());
-            dateTextView.setText(reportClusterItem.getDate());
-            // Load the image from the URL
-            imageLoader.loadImage(reportClusterItem.getImageFileName(), imageView);
-
-        }
-
-        return infoWindowView;
+        Log.d("CustomInfoWindowAdapter", "getInfoWindow");
+        render(marker, mWindow);
+        return mWindow;
     }
 
     @Override
     public View getInfoContents(Marker marker) {
-        return null; // We don't need this anymore because we use getInfoWindow()
+        return null;
+    }
+
+    private void render(Marker marker, View view) {
+        // Corrected line: Cast to ReportValidated instead of ReportClusterItem
+        ReportValidated reportValidated = (ReportValidated) marker.getTag();
+
+        if (reportValidated != null) {
+            TextView titleUi = view.findViewById(R.id.info_window_title);
+            TextView descriptionUi = view.findViewById(R.id.info_window_date);
+            ImageView imageUi = view.findViewById(R.id.info_window_image);
+
+            String title = reportValidated.getDescription();
+            if (title != null) {
+                titleUi.setText(title);
+            } else {
+                titleUi.setText("");
+            }
+
+            String description = reportValidated.getDateTimeString();
+            if (description != null) {
+                descriptionUi.setText(description);
+            } else {
+                descriptionUi.setText("");
+            }
+            imageLoader.loadImage(reportValidated.getImage(), imageUi, new ImageLoader.ImageLoadCallback() {
+                @Override
+                public void onImageLoaded() {
+                    if (marker.isInfoWindowShown()) {
+                        marker.hideInfoWindow();
+                        marker.showInfoWindow();
+                    }
+                }
+
+                @Override
+                public void onImageLoadFailed() {
+                    Log.e("CustomInfoWindowAdapter", "Failed to load image");
+                }
+            });
+        } else {
+            Log.e("CustomInfoWindowAdapter", "reportValidated is null");
+        }
     }
 }
