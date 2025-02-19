819c9f67ee60ddb405806827a50b1507c4419e16
ybitite <younes.bitite@gmail.com>
2025-02-19 02:27:10 +0100

    Feat: Implement custom info window for map markers
    - Added a custom info window layout (info_window_layout.xml).
    - Implemented CustomInfoWindowAdapter to display custom content in the info window.
    - Displayed report details (image, title, snippet, date) in the info window.
    - Used ImageLoader to asynchronously load images into the info window.
    - Associate the marker to a ReportClusterItem.

---
+++ app/src/main/java/ch/y/bitite/safespot/ui/home/CustomInfoWindowAdapter.java
@@ -0,0 +1,53 @@
+package ch.y.bitite.safespot.ui.home;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+import ch.y.bitite.safespot.R;
+import ch.y.bitite.safespot.model.ReportClusterItem;
+import ch.y.bitite.safespot.utils.ImageLoader;
+
+public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+
+    private final ImageLoader imageLoader;
+    private View infoWindowView;
+
+    public CustomInfoWindowAdapter(Context context) {
+        this.imageLoader = new ImageLoader(context);
+        this.infoWindowView = LayoutInflater.from(context).inflate(R.layout.info_window_layout, null);
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        // Get the ReportClusterItem associated with the marker
+        ReportClusterItem reportClusterItem = (ReportClusterItem) marker.getTag();
+
+        // Find the views in the layout
+        ImageView imageView = infoWindowView.findViewById(R.id.info_window_image);
+        TextView titleTextView = infoWindowView.findViewById(R.id.info_window_title);
+        TextView snippetTextView = infoWindowView.findViewById(R.id.info_window_snippet);
+        TextView dateTextView = infoWindowView.findViewById(R.id.info_window_date);
+
+        // Set the data to the views
+        if (reportClusterItem != null) {
+            // Load the image from the URL
+            imageLoader.loadImage(reportClusterItem.getImageFileName(), imageView);
+            titleTextView.setText(reportClusterItem.getTitle());
+            snippetTextView.setText(reportClusterItem.getSnippet());
+            dateTextView.setText(reportClusterItem.getDate());
+        }
+
+        return infoWindowView;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null; // We don't need this anymore because we use getInfoWindow()
+    }
+}
