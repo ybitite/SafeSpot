5e9a07795287cf8ed7aa70b02ec0e08cd5305aee
ybitite <younes.bitite@gmail.com>
2025-02-22 00:20:42 +0100

    Refactor(HomeFragment): Improve map management and data handling

--- app/src/main/java/ch/y/bitite/safespot/ui/home/HomeFragment.java
+++ app/src/main/java/ch/y/bitite/safespot/ui/home/HomeFragment.java
@@ -34,6 +34,9 @@ import ch.y.bitite.safespot.viewmodel.AddReportViewModel;
 import ch.y.bitite.safespot.viewmodel.HomeViewModel;
 import dagger.hilt.android.AndroidEntryPoint;
 
+/**
+ * Fragment for the home screen, displaying a map with report markers.
+ */
 @AndroidEntryPoint
 public class HomeFragment extends Fragment implements OnMapReadyCallback, LocationHelper.LocationCallback, HomeButtonHelper.HomeButtonCallback {
 
@@ -43,23 +46,28 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Locati
     private LocationHelper locationHelper;
     private ProgressBar progressBar;
     private HomeViewModel homeViewModel;
-    private AddReportViewModel addReportViewModel;
     private boolean isMapReady = false;
     private CustomInfoWindowAdapter customInfoWindowAdapter;
-
-
-
     private ClusterManager<ReportClusterItem> mClusterManager;
 
-
-
+    /**
+     * Called to have the fragment instantiate its user interface view.
+     *
+     * @param inflater           The LayoutInflater object that can be used to inflate
+     *                           any views in the fragment.
+     * @param container          If non-null, this is the parent view that the fragment's
+     *                           UI should be attached to.  The fragment should not add the view itself,
+     *                           but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     *                           from a previous saved state as given here.
+     * @return Return the View for the fragment's UI, or null.
+     */
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         progressBar = binding.progressBarHome;
-
 
         mapView = binding.mapView;
         mapView.onCreate(savedInstanceState);
@@ -72,54 +80,66 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Locati
         // Initialize the customInfoWindowAdapter here
         customInfoWindowAdapter = new CustomInfoWindowAdapter(getContext());
 
-
-
         return root;
     }
 
+    /**
+     * Called immediately after onCreateView() has returned, but before any saved state has been restored in to the view.
+     *
+     * @param view               The View returned by onCreateView().
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     *                           from a previous saved state as given here.
+     */
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         homeViewModel = new ViewModelProvider(requireActivity()).get(HomeViewModel.class);
-        addReportViewModel = new ViewModelProvider(requireActivity()).get(AddReportViewModel.class);
+        AddReportViewModel addReportViewModel = new ViewModelProvider(requireActivity()).get(AddReportViewModel.class);
 
         progressBar = binding.progressBarHome;
 
+        // Observe loading state of homeViewModel
         homeViewModel.getIsLoading().observe(getViewLifecycleOwner(), isLoading -> {
             Log.d("homeViewModel", "isLoading changed: " + isLoading);
             progressBar.setVisibility(isLoading ? View.VISIBLE : View.GONE);
         });
 
+        // Observe error messages from homeViewModel
         homeViewModel.getErrorMessage().observe(getViewLifecycleOwner(), errorMessage -> {
             if (errorMessage != null && homeViewModel.isDataInserted()) {
                 Toast.makeText(getContext(), errorMessage, Toast.LENGTH_SHORT).show();
             }
         });
 
-
+        // Observe loading state of addReportViewModel
         addReportViewModel.getIsLoading().observe(getViewLifecycleOwner(), isLoading -> {
             Log.d("addReportViewModel", "isLoading changed: " + isLoading);
             progressBar.setVisibility(isLoading ? View.VISIBLE : View.GONE);
         });
 
+        // Observe error messages from addReportViewModel
         addReportViewModel.getErrorMessage().observe(getViewLifecycleOwner(), errorMessage -> {
             if (errorMessage != null) {
                 Toast.makeText(getContext(), errorMessage, Toast.LENGTH_SHORT).show();
             }
         });
-
-
     }
 
+    /**
+     * Called when the map is ready to be used.
+     *
+     * @param map The GoogleMap instance.
+     */
     @Override
     public void onMapReady(@NonNull GoogleMap map) {
         Log.d("HomeFragment", "onMapReady");
         googleMap = map;
 
-        mClusterManager = new ClusterManager<>(getContext(), googleMap);
+        // Initialize the ClusterManager
+        mClusterManager = new ClusterManager<>(requireContext(), googleMap);
 
-        // Set the custom renderer
+        // Set the custom renderer for the ClusterManager
         ReportClusterItemRenderer renderer = new ReportClusterItemRenderer(getContext(), googleMap, mClusterManager);
         mClusterManager.setRenderer(renderer);
         googleMap.setOnCameraIdleListener(mClusterManager);
@@ -127,6 +147,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Locati
         // Set the custom InfoWindowAdapter to the ClusterManager
         mClusterManager.getMarkerCollection().setInfoWindowAdapter(customInfoWindowAdapter);
 
+        // Set listeners for info window clicks
         mClusterManager.setOnClusterItemInfoWindowClickListener(marker ->
                 Toast.makeText(getContext(),
                         "Info window clicked.",
@@ -136,22 +157,25 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Locati
                         "Info window long pressed.",
                         Toast.LENGTH_SHORT).show());
 
-
+        // Set initial map location and check for location permissions
         LatLng initialLocation = new LatLng(46.94809, 7.44744);
         centerMap(initialLocation);
         locationHelper.checkLocationPermissions();
 
+        // Update map markers if reports are available
         List<ReportValidated> reports = homeViewModel.getValidatedReports().getValue();
         if (reports != null) {
             updateMapMarkers(reports);
         }
 
+        // Observe current location and center the map if it changes
         homeViewModel.getCurrentLocation().observe(getViewLifecycleOwner(), location -> {
             if (location != null && isMapReady) {
                 centerMap(location);
             }
         });
 
+        // Observe validated reports and update map markers if they change
         homeViewModel.getValidatedReports().observe(getViewLifecycleOwner(), reports2 -> {
             if (isMapReady) {
                 updateMapMarkers(reports2);
@@ -160,8 +184,12 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Locati
         isMapReady = true;
     }
 
+    /**
+     * Updates the map markers with the given list of reports.
+     *
+     * @param reports The list of ReportValidated objects to display on the map.
+     */
     private void updateMapMarkers(List<ReportValidated> reports) {
-
         mClusterManager.clearItems();
         for (ReportValidated report : reports) {
             // Use ReportClusterItem to wrap the ReportValidated object
@@ -170,16 +198,28 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Locati
         mClusterManager.cluster();
     }
 
-
+    /**
+     * Centers the map on the given location.
+     *
+     * @param location The location to center the map on.
+     */
     private void centerMap(LatLng location) {
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 14));
     }
 
+    /**
+     * Called when a new location is available.
+     *
+     * @param location The new location.
+     */
     @Override
     public void onLocationResult(LatLng location) {
         homeViewModel.setCurrentLocation(location);
     }
 
+    /**
+     * Called when the center map button is clicked.
+     */
     @Override
     public void onCenterMapClicked() {
         if (isMapReady) {
@@ -192,6 +232,9 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Locati
         }
     }
 
+    /**
+     * Called when the fragment is resumed.
+     */
     @Override
     public void onResume() {
         super.onResume();
@@ -199,6 +242,9 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Locati
         mapView.onResume();
     }
 
+    /**
+     * Called when the fragment is paused.
+     */
     @Override
     public void onPause() {
         Log.d("HomeFragment", "onPause");
@@ -206,20 +252,29 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Locati
         mapView.onPause();
     }
 
+    /**
+     * Called when the fragment is destroyed.
+     */
     @Override
     public void onDestroy() {
         Log.d("HomeFragment", "onDestroy");
         super.onDestroy();
-       // mapView.onDestroy();
+        // mapView.onDestroy(); // Commented out as it might cause issues
     }
 
+    /**
+     * Called when the system is running low on memory.
+     */
     @Override
     public void onLowMemory() {
-        Log.d("HomeFragment","onLowMemory");
+        Log.d("HomeFragment", "onLowMemory");
         super.onLowMemory();
         mapView.onLowMemory();
     }
 
+    /**
+     * Called when the fragment's view is destroyed.
+     */
     @Override
     public void onDestroyView() {
         Log.d("HomeFragment", "onDestroyView");
@@ -227,6 +282,9 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Locati
         binding = null;
     }
 
+    /**
+     * Called when the add report button is clicked.
+     */
     @Override
     public void onAddReportClicked() {
         Log.d("HomeFragment", "onAddReportClicked");
@@ -234,9 +292,12 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Locati
                 .navigate(R.id.action_homeFragment_to_addReportFragment);
     }
 
+    /**
+     * Called when the refresh button is clicked.
+     */
     @Override
     public void onRefreshClicked() {
-        if(homeViewModel.getValidatedReports().getValue() != null){
+        if (homeViewModel.getValidatedReports().getValue() != null) {
             homeViewModel.fetchValidatedReports();
         }
     }
