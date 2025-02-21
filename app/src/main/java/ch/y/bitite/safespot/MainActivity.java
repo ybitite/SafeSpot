33b7a12da3d21bcc861d172240fcdb41d66d5ab4
ybitite <younes.bitite@gmail.com>
2025-02-22 00:22:09 +0100

    Refactor(MainActivity): - Used `Objects.requireNonNull` for safety when accessing the NavController. - Changed `BottomNavigationView` setup to use `NavigationUI.setupWithNavController`. - Improved code structure and readability.

--- app/src/main/java/ch/y/bitite/safespot/MainActivity.java
+++ app/src/main/java/ch/y/bitite/safespot/MainActivity.java
@@ -2,42 +2,46 @@ package ch.y.bitite.safespot;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import java.util.Objects;
 
 import ch.y.bitite.safespot.databinding.ActivityMainBinding;
 import dagger.hilt.android.AndroidEntryPoint;
 
-@AndroidEntryPoint // Ajouter l'annotation ici
-
+/**
+ * The main activity of the SafeSpot application.
+ * This activity serves as the entry point and sets up the navigation.
+ */
+@AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
-
+    /**
+     * Called when the activity is first created.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in {@link #onSaveInstanceState}. Otherwise, it is null.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        // Inflate the layout using View Binding
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.homeFragment, R.id.dashboardFragment)
-                .build();
+        // Retrieve the NavHostFragment
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.nav_host_fragment_activity_main);
-        NavController navController = navHostFragment.getNavController();
 
+        // Get the NavController from the NavHostFragment
+        NavController navController = Objects.requireNonNull(navHostFragment).getNavController();
+
+        // Set up the BottomNavigationView with the NavController
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
-
 }
