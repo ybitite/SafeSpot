f9a754a8ceb42d59bd318110aa79911d744fa003
ybitite <younes.bitite@gmail.com>
2025-02-22 00:23:46 +0100

    Refactor: Add Javadoc to RetrofitClient
    - Added Javadoc comments to the `RetrofitClient` class and its methods to improve documentation and readability.
    - Added documentation for `createRetrofit`, `createOkHttpClient`, `createLoggingInterceptor`, and `createGson`.
    - Clarified the purpose of the `RetrofitClient` class.

--- app/src/main/java/ch/y/bitite/safespot/network/RetrofitClient.java
+++ app/src/main/java/ch/y/bitite/safespot/network/RetrofitClient.java
@@ -11,9 +11,20 @@ import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+/**
+ * This class provides utility methods for creating Retrofit, OkHttpClient,
+ * HttpLoggingInterceptor, and Gson instances.
+ */
 public class RetrofitClient {
     private static final String TAG = "RetrofitClient";
 
+    /**
+     * Creates a Retrofit instance.
+     *
+     * @param client The OkHttpClient to use.
+     * @param gson   The Gson instance to use for JSON conversion.
+     * @return A Retrofit instance.
+     */
     public static Retrofit createRetrofit(OkHttpClient client, Gson gson) {
         return new Retrofit.Builder()
                 .baseUrl(BuildConfig.BASE_URL)
@@ -22,18 +33,34 @@ public class RetrofitClient {
                 .build();
     }
 
+    /**
+     * Creates an OkHttpClient instance.
+     *
+     * @param logging The HttpLoggingInterceptor to add to the client.
+     * @return An OkHttpClient instance.
+     */
     public static OkHttpClient createOkHttpClient(HttpLoggingInterceptor logging) {
         return new OkHttpClient.Builder()
                 .addInterceptor(logging)
                 .build();
     }
 
+    /**
+     * Creates an HttpLoggingInterceptor instance.
+     *
+     * @return An HttpLoggingInterceptor instance.
+     */
     public static HttpLoggingInterceptor createLoggingInterceptor() {
         HttpLoggingInterceptor logging = new HttpLoggingInterceptor(message -> Log.d(TAG, "log: " + message));
         logging.setLevel(HttpLoggingInterceptor.Level.BODY);
         return logging;
     }
 
+    /**
+     * Creates a Gson instance.
+     *
+     * @return A Gson instance.
+     */
     public static Gson createGson() {
         return new GsonBuilder()
                 .create();
