004067b1835802e43edb16f2e901e99cd846f900
ybitite <younes.bitite@gmail.com>
2025-02-22 00:13:12 +0100

    Refactor(AdapterModule): Update ReportAdapter dependency injection
    - Updated the comment for the AdapterModule class to explain its purpose.
    - Added comments to the provideReportAdapter method explaining its functionality.

--- app/src/main/java/ch/y/bitite/safespot/di/AdapterModule.java
+++ app/src/main/java/ch/y/bitite/safespot/di/AdapterModule.java
@@ -1,7 +1,5 @@
 package ch.y.bitite.safespot.di;
 
-import com.bumptech.glide.RequestManager;
-
 import ch.y.bitite.safespot.ui.dashboard.ReportAdapter;
 import ch.y.bitite.safespot.utils.ImageLoader;
 import dagger.Module;
@@ -10,10 +8,20 @@ import dagger.hilt.InstallIn;
 import dagger.hilt.android.components.FragmentComponent;
 import dagger.hilt.android.scopes.FragmentScoped;
 
+/**
+ * This module provides dependencies related to adapters, specifically the ReportAdapter.
+ * It uses Dagger Hilt for dependency injection.
+ */
 @Module
 @InstallIn(FragmentComponent.class)
 public class AdapterModule {
 
+    /**
+     * Provides an instance of ReportAdapter.
+     *
+     * @param imageLoader The ImageLoader instance to be injected into the ReportAdapter.
+     * @return An instance of ReportAdapter.
+     */
     @Provides
     @FragmentScoped
     public ReportAdapter provideReportAdapter(ImageLoader imageLoader) {
