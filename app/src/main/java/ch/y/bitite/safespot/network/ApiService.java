a615de8cb0bdd8c68b87e655083409d3cdf7df5d
ybitite <younes.bitite@gmail.com>
2025-02-22 00:14:26 +0100

    Refactor(ApiService): Add Javadoc for API methods
    - Added Javadoc comments to the `getValidatedReports` and `addReport` methods in `ApiService` to improve code documentation.
    - The Javadoc explains what each method does, what parameters it takes, and what it returns.

--- app/src/main/java/ch/y/bitite/safespot/network/ApiService.java
+++ app/src/main/java/ch/y/bitite/safespot/network/ApiService.java
@@ -11,10 +11,28 @@ import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
 
+/**
+ * Interface defining the API service for network requests.
+ */
 public interface ApiService {
+    /**
+     * Retrieves a list of validated reports.
+     *
+     * @return A Call object containing a list of ReportValidated.
+     */
     @GET("api/reports")
     Call<List<ReportValidated>> getValidatedReports();
 
+    /**
+     * Adds a new report.
+     *
+     * @param image       The image part of the report.
+     * @param description The description of the report.
+     * @param longitude   The longitude of the report location.
+     * @param latitude    The latitude of the report location.
+     * @param date_Time   The date and time of the report.
+     * @return A Call object.
+     */
     @Multipart
     @POST("api/reports")
     Call<Void> addReport(
