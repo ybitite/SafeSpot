8e7bece76817231a3a94ace044a79b7f06398342
ybitite <younes.bitite@gmail.com>
2025-02-21 03:20:25 +0100

    Refactor(ReportRepository): Simplify data fetching and storage
    - Removed unused SharedPreferences logic.
    - Removed unnecessary MediatorLiveData.
    - Simplified data fetching logic.
    - Removed unused isFirstFetch flag.
    - The database state and it's checking is removed.

--- app/src/main/java/ch/y/bitite/safespot/repository/ReportRepository.java
+++ app/src/main/java/ch/y/bitite/safespot/repository/ReportRepository.java
@@ -1,13 +1,9 @@
 package ch.y.bitite.safespot.repository;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.util.Log;
 
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MediatorLiveData;
-import androidx.lifecycle.Observer;
 
 import java.io.File;
 import java.util.List;
@@ -24,18 +20,9 @@ import ch.y.bitite.safespot.model.ReportValidated;
 public class ReportRepository {
 
     private static final String TAG = "ReportRepository";
-    private static final String PREFS_NAME = "ReportPrefs";
-    private static final String IS_DATABASE_EMPTY_KEY = "isDatabaseEmpty";
     private final ReportLocalDataSource localDataSource;
     private final ReportRemoteDataSource remoteDataSource;
     private final ReportFileDataSource fileDataSource;
-    private final MediatorLiveData<List<ReportValidated>> mediatorLiveData = new MediatorLiveData<>();
-    private boolean isDatabaseEmpty;
-
-
-
-    private boolean isFirstFetch = true;
-    private final SharedPreferences sharedPreferences;
 
     /**
      * Constructor for ReportRepository.
@@ -43,63 +30,33 @@ public class ReportRepository {
      * @param localDataSource  The ReportLocalDataSource for database operations.
      * @param remoteDataSource The ReportRemoteDataSource for network requests.
      * @param fileDataSource   The ReportFileDataSource for file operations.
-     * @param context          The application context.
      */
     @Inject
-    public ReportRepository(ReportLocalDataSource localDataSource, ReportRemoteDataSource remoteDataSource, ReportFileDataSource fileDataSource, Context context) {
-
+    public ReportRepository(ReportLocalDataSource localDataSource, ReportRemoteDataSource remoteDataSource, ReportFileDataSource fileDataSource) {
         this.localDataSource = localDataSource;
         this.remoteDataSource = remoteDataSource;
         this.fileDataSource = fileDataSource;
-        this.sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        // Load the database state from SharedPreferences
-        isDatabaseEmpty = sharedPreferences.getBoolean(IS_DATABASE_EMPTY_KEY, true);
-        // Observe changes in the local database
-        LiveData<List<ReportValidated>> localData = localDataSource.getAllValidatedReports();
-        mediatorLiveData.addSource(localData, new Observer<List<ReportValidated>>() {
-            @Override
-            public void onChanged(List<ReportValidated> reportValidateds) {
-                mediatorLiveData.setValue(reportValidateds);
-                // Update the database state and save it to SharedPreferences
-                boolean newIsDatabaseEmpty = reportValidateds.isEmpty();
-                if (isDatabaseEmpty != newIsDatabaseEmpty) {
-                    isDatabaseEmpty = newIsDatabaseEmpty;
-                    sharedPreferences.edit().putBoolean(IS_DATABASE_EMPTY_KEY, isDatabaseEmpty).apply();
-                }
-            }
-        });
     }
 
-    /**
-     * Retrieves all validated reports from the local database.
-     *
-     * @return A LiveData list of ReportValidated objects.
-     */
     public LiveData<List<ReportValidated>> getAllValidatedReports() {
-        return mediatorLiveData;
+        return localDataSource.getAllValidatedReports();
     }
+
 
     /**
      * Fetches validated reports from the API and updates the local database.
      */
     public void fetchValidatedReports(FetchValidatedReportsCallback callback) {
-
         remoteDataSource.fetchValidatedReports(new ReportRemoteDataSource.FetchValidatedReportsCallback() {
             @Override
             public void onSuccess(List<ReportValidated> reports) {
                 localDataSource.insertValidatedReports(reports);
                 callback.onSuccess(reports);
-                isFirstFetch = false;
             }
 
             @Override
             public void onFailure(String errorMessage) {
-                if (isDatabaseEmpty || isFirstFetch) {
-                    callback.onFailure(errorMessage);
-                } else {
-                    callback.onSuccess(mediatorLiveData.getValue());
-                }
-                isFirstFetch = false;
+                callback.onFailure(errorMessage);
             }
         });
     }
@@ -127,15 +84,15 @@ public class ReportRepository {
             }
         });
     }
-    public boolean isFirstFetch() {
-        return isFirstFetch;
-    }
+
     public interface FetchValidatedReportsCallback {
         void onSuccess(List<ReportValidated> reports);
-
         void onFailure(String errorMessage);
     }
 
+    /**
+     * Callback interface for the addReport operation.
+     */
     public interface AddReportCallback {
         void onSuccess(String message);
 
