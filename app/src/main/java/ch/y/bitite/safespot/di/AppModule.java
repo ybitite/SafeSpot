fcb1e72489b77d498df03f3837e053ece787e5c0
ybitite <younes.bitite@gmail.com>
2025-02-22 00:16:10 +0100

    Refactor(AppModule): Add Javadoc

--- app/src/main/java/ch/y/bitite/safespot/di/AppModule.java
+++ app/src/main/java/ch/y/bitite/safespot/di/AppModule.java
@@ -14,10 +14,19 @@ import dagger.hilt.InstallIn;
 import dagger.hilt.android.qualifiers.ApplicationContext;
 import dagger.hilt.components.SingletonComponent;
 
+/**
+ * This module provides application-wide dependencies using Dagger Hilt.
+ */
 @Module
 @InstallIn(SingletonComponent.class)
 public class AppModule {
 
+    /**
+     * Provides a singleton instance of RequestManager for image loading with Glide.
+     *
+     * @param context The application context.
+     * @return A RequestManager instance.
+     */
     @Provides
     @Singleton
     public RequestManager provideGlide(@ApplicationContext Context context) {
@@ -36,6 +45,12 @@ public class AppModule {
         return context;
     }
 
+    /**
+     * Provides a singleton instance of ImageLoader.
+     *
+     * @param context The application context.
+     * @return An ImageLoader instance.
+     */
     @Provides
     @Singleton
     public ImageLoader provideImageLoader(@ApplicationContext Context context) {
