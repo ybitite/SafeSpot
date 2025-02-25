1164c6ca3db213b90026bf5c4f756026e7d1095e
ybitite <younes.bitite@gmail.com>
2025-02-25 06:30:05 +0100

    Feat: Add Glide module
    - Added the MyAppGlideModule class to manage Glide configurations.

---
+++ app/src/main/java/ch/y/bitite/safespot/MyAppGlideModule.java
@@ -0,0 +1,8 @@
+package ch.y.bitite.safespot;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class MyAppGlideModule extends AppGlideModule {
+}
