cf29092202d7aeb9fe5304c0dcb631198c69e694
ybitite <younes.bitite@gmail.com>
2025-02-25 06:29:10 +0100

    Refactor(CustomInfoWindowAdapter): Improve info window display and image loading
    - Removed unnecessary comments to improve code readability.
    - Set the info window anchor to adjust the position of the info window.
    - Formatted the report date by replacing "T" with a space.
    - Refresh the info window only if it is shown after an image has been loaded.
    - Added a check on the marker.isInfoWindowShown() when refreshing the info window after image is loaded.

--- app/src/main/java/ch/y/bitite/safespot/ui/home/CustomInfoWindowAdapter.java
+++ app/src/main/java/ch/y/bitite/safespot/ui/home/CustomInfoWindowAdapter.java
@@ -17,57 +17,31 @@ import ch.y.bitite.safespot.R;
 import ch.y.bitite.safespot.model.ReportClusterItem;
 import ch.y.bitite.safespot.utils.ImageLoader;
 
-/**
- * Custom InfoWindowAdapter for displaying custom info windows on Google Map markers.
- */
 public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
     private final View mWindow;
     private final ImageLoader imageLoader;
 
-    /**
-     * Constructor for CustomInfoWindowAdapter.
-     *
-     * @param context The application context.
-     */
     @SuppressLint("InflateParams")
     public CustomInfoWindowAdapter(Context context) {
         this.imageLoader = new ImageLoader(context);
         mWindow = LayoutInflater.from(context).inflate(R.layout.info_window_layout, null);
     }
 
-    /**
-     * Provides the custom info window for a marker.
-     *
-     * @param marker The marker for which to provide the info window.
-     * @return The custom info window view.
-     */
     @Override
     public View getInfoWindow(@NonNull Marker marker) {
         Log.d("CustomInfoWindowAdapter", "getInfoWindow");
+        marker.setInfoWindowAnchor(0.5f, 5.0f);
         render(marker, mWindow);
         return mWindow;
     }
 
-    /**
-     * Provides the content view for the info window.
-     *
-     * @param marker The marker for which to provide the info window content.
-     * @return Null, as we are using a custom info window.
-     */
     @Override
     public View getInfoContents(@NonNull Marker marker) {
         return null;
     }
 
-    /**
-     * Renders the content of the info window.
-     *
-     * @param marker The marker associated with the info window.
-     * @param view   The view to render the content into.
-     */
     private void render(Marker marker, View view) {
-        // Get the ReportClusterItem from the Marker's tag
         ReportClusterItem reportClusterItem = (ReportClusterItem) marker.getTag();
 
         if (reportClusterItem != null) {
@@ -83,14 +57,18 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
             String date = reportClusterItem.getDate();
             if (date != null) {
-                descriptionUi.setText(date);
+                String formattedDate = date.replace("T", " ");
+                descriptionUi.setText(formattedDate);
             } else {
                 descriptionUi.setText("");
             }
+
             imageLoader.loadImage(reportClusterItem.getImageFileName(), imageUi, new ImageLoader.ImageLoadCallback() {
                 @Override
                 public void onImageLoaded() {
-                    // Refresh the info window to display the loaded image
+                    // Notify the info window that the content has changed
+
+
                     if (marker.isInfoWindowShown()) {
                         marker.hideInfoWindow();
                         marker.showInfoWindow();
@@ -100,6 +78,7 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
                 @Override
                 public void onImageLoadFailed() {
                     Log.e("CustomInfoWindowAdapter", "Failed to load image");
+
                 }
             });
         } else {
