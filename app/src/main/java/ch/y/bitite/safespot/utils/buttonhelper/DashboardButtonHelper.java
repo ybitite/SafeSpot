d06bafe00782e166c7243dc512c5c8fc5c6de142
ybitite <younes.bitite@gmail.com>
2025-02-27 05:45:40 +0100

    Refactor: Update dashboard button handling
    - Changed `buttonAddReport` and `refreshButton` to `FloatingActionButton`.
    - Added `sortButton` to handle sorting functionality.
    - Introduced `onSortClicked()` to DashboardButtonCallback.
    - Added a listener for `sortButton` and calls `onSortClicked()` when the sort button is clicked.

--- app/src/main/java/ch/y/bitite/safespot/utils/buttonhelper/DashboardButtonHelper.java
+++ app/src/main/java/ch/y/bitite/safespot/utils/buttonhelper/DashboardButtonHelper.java
@@ -1,7 +1,10 @@
 package ch.y.bitite.safespot.utils.buttonhelper;
 
 import android.view.View;
-import android.widget.Button;
+import android.widget.Button; // Importation de Button
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import ch.y.bitite.safespot.R;
 
@@ -9,9 +12,10 @@ import ch.y.bitite.safespot.R;
  * Helper class for managing button interactions in the DashboardFragment.
  */
 public class DashboardButtonHelper {
-    private final Button buttonAddReport;
-    private final Button refreshButton;
+    private final FloatingActionButton buttonAddReport;
+    private final FloatingActionButton refreshButton;
     private final DashboardButtonCallback dashboardButtonCallback;
+    private final Button sortButton; // Déclaration du bouton de tri
 
     /**
      * Constructor for DashboardButtonHelper.
@@ -23,28 +27,15 @@ public class DashboardButtonHelper {
         this.dashboardButtonCallback = callback;
         buttonAddReport = root.findViewById(R.id.buttonAddReport);
         refreshButton = root.findViewById(R.id.refreshButton);
+        sortButton = root.findViewById(R.id.sort_by_date_button);
     }
 
     /**
-     * Sets up the click listeners for the add report and refresh buttons.
+     * Sets up the click listeners for the add report, refresh, and sort buttons.
      */
     public void setupDashboardButtonListeners() {
         buttonAddReport.setOnClickListener(v -> dashboardButtonCallback.onAddReportClicked());
         refreshButton.setOnClickListener(v -> dashboardButtonCallback.onRefreshClicked());
-    }
-
-    /**
-     * Callback interface for button clicks.
-     */
-    public interface DashboardButtonCallback {
-        /**
-         * Called when the add report button is clicked.
-         */
-        void onAddReportClicked();
-
-        /**
-         * Called when the refresh button is clicked.
-         */
-        void onRefreshClicked();
+        sortButton.setOnClickListener(v -> dashboardButtonCallback.onSortClicked());
     }
 }
